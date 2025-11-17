f62ba7afe50e6954e582ca72cc66d798de40cef2
AhmedAvdihodzic <ahmedavdihodzic@gmail.com>
2025-11-17 08:31:04 +0100

    feat: comments

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -1,4 +1,3 @@
-
 class Scanner
   def initialize(radar, invader, threshold: 80)
     @radar = radar
@@ -10,21 +9,27 @@ class Scanner
     results = []
     y_range = (-@invader.height + 1)...@radar.height
     x_range = (-@invader.width + 1)...@radar.width
+    #defines the range that includes every possible overlap of the invader and the radar
 
     y_range.each do |y|
       x_range.each do |x|
         on_edge = false
         top = [0, y].max
         left = [0, x].max
+        #determines the start of the window (always non-negative)
 
         bottom = [@radar.height, y + @invader.height].min
         right  = [@radar.width, x + @invader.width].min
+        #determines the end of the window (must not exceed radar size)
 
         on_edge = y < 0 || x < 0 || ((y + @invader.height) > @radar.height) || 
                                     ((x + @invader.width) > @radar.width)
+        #sets on_edge to true if the invader is partially outside the radar
 
         h = bottom - top
         w = right - left
+        #determines the window's height and width
+
         next if h <= 0 || w <= 0
 
         visible_cells = h * w
@@ -33,15 +38,19 @@ class Scanner
         if on_edge && visible_cells < total_cells * 0.5
           next
         end
+        #skips records if more than half of the window is outside the radar
 
         window = @radar.window(left, top, w, h)
-
         inv_slice = @invader.pattern[top - y, h].map { |r| r[left - x, w] }
+        #cuts out the radar and invader windows to the required dimensions
 
         confidence = ConfidenceChecker.match(window, inv_slice)
+        #compares the two windows
+
         if confidence >= @threshold
           results.push(ScanResult.new(@invader, x, y, confidence.to_i.to_s, on_edge))
         end
+        #if threshold is met, pushes the record
       end
     end
 
